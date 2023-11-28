287977fabb7020a389dc95eb453967fde2c6075d
noah-bar <noah.barberini@cpnv.ch>
2023-11-28 10:27:20 +0100

    chore: Change context type

--- app/Controllers/Http/Api/V1/WorkspacesController.ts
+++ app/Controllers/Http/Api/V1/WorkspacesController.ts
@@ -1,15 +1,15 @@
-import { HttpContext } from '@adonisjs/core/build/standalone'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Workspace from 'App/Models/Workspace'
 import CreateWorkspaceValidator from 'App/Validators/CreateWorkspaceValidator'
 import UpdateWorkspaceValidator from 'App/Validators/UpdateWorkspaceValidator'
 
 export default class WorkspacesController {
-  public async index({} : HttpContext) {
+  public async index({} : HttpContextContract) {
     const workspaces = await Workspace.all()
     return workspaces
   }
 
-  public async show({ params, response }: HttpContext) {
+  public async show({ params, response }: HttpContextContract) {
     const workspace = await Workspace.find(params.id)
     if(!workspace) {
       return response.notFound()
@@ -17,13 +17,13 @@ export default class WorkspacesController {
     return workspace
   }
 
-  public async store({ request }: HttpContext) {
+  public async store({ request }: HttpContextContract) {
     const payload = await request.validate(CreateWorkspaceValidator)
     const workspace = await Workspace.create(payload)
     return workspace
   }
 
-  public async update({ response, request, params }: HttpContext) {
+  public async update({ response, request, params }: HttpContextContract) {
     const payload = await request.validate(UpdateWorkspaceValidator)
     const workspace = await Workspace.find(params.id)
     if(!workspace) {
@@ -33,7 +33,7 @@ export default class WorkspacesController {
     return workspace
   }
 
-  public async destroy({params, response}: HttpContext) {
+  public async destroy({params, response}: HttpContextContract) {
     const workspace = await Workspace.find(params.id)
     if(!workspace) {
       return response.notFound()
