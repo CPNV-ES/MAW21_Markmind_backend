353cdf6db3fe679c5452de01d5d648784c7fab92
Hyilbo <boris.shaffner@gmail.com>
2023-12-07 11:27:45 +0100

    feat: preload workspaces relations

--- app/Controllers/Http/Api/V1/WorkspacesController.ts
+++ app/Controllers/Http/Api/V1/WorkspacesController.ts
@@ -1,46 +1,49 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Workspace from 'App/Models/Workspace'
-import CreateWorkspaceValidator from 'App/Validators/CreateWorkspaceValidator'
-import UpdateWorkspaceValidator from 'App/Validators/UpdateWorkspaceValidator'
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import Workspace from "App/Models/Workspace";
+import CreateWorkspaceValidator from "App/Validators/CreateWorkspaceValidator";
+import UpdateWorkspaceValidator from "App/Validators/UpdateWorkspaceValidator";
 
 export default class WorkspacesController {
-  public async index({} : HttpContextContract) {
-    const workspaces = await Workspace.all()
-    return workspaces
+  public async index({}: HttpContextContract) {
+    const workspaces = await Workspace.all();
+    return workspaces;
   }
 
   public async show({ params, response }: HttpContextContract) {
-    const workspace = await Workspace.find(params.id)
-    if(!workspace) {
-      return response.notFound()
+    const workspace = await Workspace.query()
+      .where("id", params.id)
+      .preload("collections", (collection) => {
+        collection.preload("resources");
+      });
+
+    if (!workspace) {
+      return response.notFound();
     }
-    await workspace.load('collections')
-    return workspace
+    return workspace;
   }
 
   public async store({ request }: HttpContextContract) {
-    const payload = await request.validate(CreateWorkspaceValidator)
-    const workspace = await Workspace.create(payload)
-    return workspace
+    const payload = await request.validate(CreateWorkspaceValidator);
+    const workspace = await Workspace.create(payload);
+    return workspace;
   }
 
   public async update({ response, request, params }: HttpContextContract) {
-    const payload = await request.validate(UpdateWorkspaceValidator)
-    const workspace = await Workspace.find(params.id)
-    if(!workspace) {
-      return response.notFound()
+    const payload = await request.validate(UpdateWorkspaceValidator);
+    const workspace = await Workspace.find(params.id);
+    if (!workspace) {
+      return response.notFound();
     }
-    await workspace.merge(payload).save()
-    return workspace
+    await workspace.merge(payload).save();
+    return workspace;
   }
 
-  public async destroy({params, response}: HttpContextContract) {
-    const workspace = await Workspace.find(params.id)
-    if(!workspace) {
-      return response.notFound()
+  public async destroy({ params, response }: HttpContextContract) {
+    const workspace = await Workspace.find(params.id);
+    if (!workspace) {
+      return response.notFound();
     }
-    await workspace.delete()
-    return response.status(204)
+    await workspace.delete();
+    return response.status(204);
   }
-
 }
