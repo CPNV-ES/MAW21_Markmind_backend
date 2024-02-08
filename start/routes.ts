431939a7be780c03667d91fc612f26b3fc7ca423
noah-bar <noah.barberini@eduvaud.ch>
2024-02-08 14:11:14 +0100

    feat: add auth middleware to secure routes

--- start/routes.ts
+++ start/routes.ts
@@ -7,10 +7,15 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-  Route.resource('workspaces', 'Api/V1/WorkspacesController').apiOnly()
-  Route.resource('collections', 'Api/V1/CollectionsController').apiOnly()
-  Route.resource('resources', 'Api/V1/ResourcesController').apiOnly()
-  Route.get('/resources/:id/markdown', 'Api/V1/ResourcesController.markdown')
+
+  Route.group(() => {
+    Route.resource('workspaces', 'Api/V1/WorkspacesController').apiOnly()
+    Route.resource('collections', 'Api/V1/CollectionsController').apiOnly()
+    Route.resource('resources', 'Api/V1/ResourcesController').apiOnly()
+    Route.get('/resources/:id/markdown', 'Api/V1/ResourcesController.markdown')
+  })
+  .middleware(['auth'])
+
   Route.post('/login', 'Api/V1/AuthController.login')
   Route.post('/register', 'Api/V1/AuthController.register')
   Route.post('/logout', 'Api/V1/AuthController.logout')
